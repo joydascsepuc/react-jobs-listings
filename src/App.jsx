7dca00114196d7ebee2d05590f9080bff4f7a109
Joy Das <joydascsepuc@gmail.com>
2024-03-23 15:24:10 +0400

    Home page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobsListings from "./components/JobsListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <HomeCards />
+            <JobsListings />
+            <ViewAllJobs />
+        </>
+    );
+};
 
-export default App
+export default App;
